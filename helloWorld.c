0ca25a42501e2f3d494c467dfc776919868be2e9
Brian Hanford <bhanford@purdue.edu>
2016-03-07 12:25:05 -0500

    Initial commit

---
+++ helloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  //input output library
+#include <stdlib.h> //standad C library
+
+int main(int argc, char ** argv) {   //main function MUST be exactly the same
+  printf("Hello World!\n"); 	     //statement to print Hello World!
+  return 0;			     //statement to return back out of this function
+} 				     //closes the function
