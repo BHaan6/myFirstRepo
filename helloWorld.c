613a2e8262edf1efe86bfbf77d2df41e7326dc4a
Brian Hanford <bhanford@purdue.edu>
2016-03-07 19:53:02 -0500

    Code was greeting the world. Now showing off our math skills with a new print statement

--- helloWorld.c
+++ helloWorld.c
@@ -3,5 +3,8 @@
 
 int main(int argc, char ** argv) {   //main function MUST be exactly the same
   printf("Hello World!\n"); 	     //statement to print Hello World!
+
+  printf("2 + 2 = 4\n");		// $$$ Correct changes $$$
+
   return 0;			     //statement to return back out of this function
 } 				     //closes the function
